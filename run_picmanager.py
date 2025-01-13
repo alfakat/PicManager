713d32ab0a568fc2edb053b0f2e82866cc3ce5d8
Kate Alfandari <44461784+alfakat@users.noreply.github.com>
2025-01-13 19:54:17 +0200

    PicManager redesign
    - changed the appearance to tabs created from csv
    - added label field for easy annotation

--- run_picmanager.py
+++ run_picmanager.py
@@ -1,10 +1,8 @@
 import os
 import argparse
 import datetime
-import threading
 from csv_creator import CSVCreator
-from gradio_ui import GradioViewer
-from multiprocessing import Pool
+from gradio_viewer import GradioViewer
 
 
 def argument_parsing() -> argparse.Namespace:
@@ -26,8 +24,11 @@ if __name__ == '__main__':
     create_initial_images_csv = CSVCreator(images_folder = args.images_folder,
                                            output_folder = args.output)
 
+    batch_list = []
     for batch in os.listdir(create_initial_images_csv.result):
         batch_path = os.path.join(create_initial_images_csv._batch_dir, batch)
-        GradioViewer(images_csv=batch_path, id=batch[:-4][12:], output=args.output)
+        batch_list.append(batch_path)
+
+    GradioViewer(batch_list=batch_list)
 
 
